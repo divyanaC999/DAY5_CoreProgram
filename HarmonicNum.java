3f4d0abd786920319b276a8f53a9668a0fe395be
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-08 14:59:07 +0530

    Added: Print Harmonic Number

---
+++ HarmonicNum.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+import java.util.*;
+public class HarmonicNum {
+      		public static void main(String[] args) {
+			    System.out.println("Enter the value of N:");
+			   Scanner sc = new Scanner(System.in);
+			   double N = sc.nextDouble();
+			    double HarmonNum = 0;
+			   for(double i = 1; i <= N; i++) {
+				    HarmonNum = HarmonNum+(1/i);
+			   }
+			    System.out.println("Nth Harmonic value is " +HarmonNum);
+			    sc.close();
+		}
+
+	}
